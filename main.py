738e04ab47d88fa899556861c504dc0ea0df5af6
Lev Kadatskyi <levkadatscky@gmail.com>
2025-01-08 14:27:41 +0200

    added game of life logic

--- main.py
+++ main.py
@@ -1,32 +1,64 @@
-import time
+import random
 
 import pygame as p
-import random
-from pygame import locals
+from pygame.locals import *
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+# create window
+root = p.display.set_mode((500, 500))
 
-root = p.display.set_mode((1000, 500))
+cells = [
+    [random.choice([0, 1]) for j in range(root.get_width() // 20)]
+    for i in range(root.get_height() // 20)
+]
 
-while True:
+
+# Function for counting the neighbours
+def near(
+    pos: list,
+    system=[[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]],
+):
+    count = 0
+    for i in system:
+        if cells[(pos[0] + i[0]) % len(cells)][(pos[1] + i[1]) % len(cells[0])]:
+            count += 1
+    return count
+
+
+while 1:
     root.fill(WHITE)
 
+    # Grid
     for i in range(0, root.get_height() // 20):
-        p.draw.line(
-            root,
-            BLACK,
-            (
-                0,
-                i * 20,
-            ),
-            (root.get_width(), i * 20),
-        )
+        p.draw.line(root, BLACK, (0, i * 20), (root.get_width(), i * 20))
     for j in range(0, root.get_width() // 20):
         p.draw.line(root, BLACK, (j * 20, 0), (j * 20, root.get_height()))
-
     for i in p.event.get():
-        if i.type == locals.QUIT:
+        if i.type == QUIT:
             quit()
 
+    # loop through all the cells
+    for i in range(0, len(cells)):
+        for j in range(0, len(cells[i])):
+            print(cells[i][j], i, j)
+            p.draw.rect(root, (255 * cells[i][j] % 256, 0, 0), [i * 20, j * 20, 20, 20])
+    # Update screen
     p.display.update()
+    cells2 = [[0 for j in range(len(cells[0]))] for i in range(len(cells))]
+    for i in range(len(cells)):
+        for j in range(len(cells[0])):
+            # if cell is alive
+            if cells[i][j]:
+                # if num of neighbours is not 2 or 3
+                if near([i, j]) not in (2, 3):
+                    cells2[i][j] = 0
+                    continue
+                cells2[i][j] = 1
+                continue
+            # if cell is dead and has 3 neighbours
+            if near([i, j]) == 3:
+                cells2[i][j] = 1
+                continue
+            cells2[i][j] = 0
+    cells = cells2
